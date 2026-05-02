ea8ed1cb72c33eaacbb58ba067f354dcf6a055db
bigfatbaloney <s.cote87@gmail.com>
2026-05-02 09:27:12 -0400

    Create Users.js

---
+++ src/Users.js
@@ -0,0 +1,38 @@
+export const USERS = [
+  {
+    username: "stephane",
+    password: "granm2026",
+    email: "stephane@lagrandmontoise.ca",
+    nom: "Stéphane",
+    role: "admin",
+    prefs: {
+      notifCalendrier: true,
+      notifEvenement: true,
+      notifMembres: false,
+    }
+  },
+  {
+    username: "marie",
+    password: "marie2026",
+    email: "marie@lagrandmontoise.ca",
+    nom: "Marie",
+    role: "membre",
+    prefs: {
+      notifCalendrier: true,
+      notifEvenement: false,
+      notifMembres: false,
+    }
+  },
+  {
+    username: "jf",
+    password: "jf2026",
+    email: "jf@lagrandmontoise.ca",
+    nom: "Jean-François",
+    role: "membre",
+    prefs: {
+      notifCalendrier: false,
+      notifEvenement: false,
+      notifMembres: false,
+    }
+  },
+];
